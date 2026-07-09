e031baacfd07613aab3ebeb11c118e01c1d44666
Bogdan <bogdanfletchers@gmail.com>
2026-07-08 22:35:27 -0600

    Domain: add Account and Transaction structs

---
+++ internal/domain/structs.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type Account struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	Balance   int64     `json:"balance"`
+	Currency  string    `json:"currency"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Transaction struct {
+	ID            string    `json:"id"`
+	FromAccountID string    `json:"from_account_id"`
+	ToAccountID   string    `json:"to_account_id"`
+	Amount        int64     `json:"amount"`
+	Status        string    `json:"status"`
+	CreatedAt     time.Time `json:"created_at"`
+}
