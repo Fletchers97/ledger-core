68abfb0d78ab927be946bb3b85affed6afa6d5c9
Bogdan <bogdanfletchers@gmail.com>
2026-07-08 23:30:04 -0600

    Main: implement test flow for AccountService

--- cmd/ledger/main.go
+++ cmd/ledger/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/Fletchers97/ledger-core/internal/domain"
+	"github.com/Fletchers97/ledger-core/internal/repository"
+	"github.com/Fletchers97/ledger-core/internal/service"
+)
 
 func main() {
-	fmt.Println("Ledger-Core Engine successfully started!")
+	repo := repository.NewInMemoryAccountRepo()
+	
+	
+	svc := service.NewAccountService(repo)
+
+	//Create a new account
+	acc := &domain.Account{
+		ID:       "1",
+		Balance:  1000, // 10 dollars in cents
+		Currency: "CAD",
+	}
+
+	
+	err := svc.CreateAccount(context.Background(), acc)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	
+	foundAcc, err := svc.GetAccountByID(context.Background(), "1")
+	if err != nil {
+    	fmt.Println("Error searching:", err)
+	} else {
+    	fmt.Println("Account found:", foundAcc.ID, "with balance:", foundAcc.Balance)
+	}
+
+	fmt.Println("Success! Account created with balance:", acc.Balance)
 }
--- internal/service/account_service.go
+++ internal/service/account_service.go
@@ -18,9 +18,13 @@ func NewAccountService(repo repository.AccountRepository) *AccountService {
 
 // CreateAccount creates a new account
 func (s *AccountService) CreateAccount(ctx context.Context, acc *domain.Account) error {
-	// Здесь можно добавить проверку: например, нельзя создать счет с отрицательным балансом
 	if acc.Balance < 0 {
-		return domain.ErrInvalidBalance // Потребуется добавить этот тип ошибки в domain
+		return domain.ErrInvalidBalance 
 	}
 	return s.repo.Create(ctx, acc)
+}
+
+// GetAccountByID retrieves an account by its ID
+func (s *AccountService) GetAccountByID(ctx context.Context, id string) (*domain.Account, error) {
+    return s.repo.GetByID(ctx, id)
 }
